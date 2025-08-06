4b82112bd3772d7b60968d7cbad90e060ea718a4
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-08-06 22:07:21 +0300

    Add create model

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-// sync db by model
-const { sequelize } = require("./models");
+const { sequelize, Student } = require("./models");
 
-sequelize.sync({force:true})
-.then(()=>'Sync OR')
-.catch((err)=>console.log(err))
+(async function () {
+  try {
+    // Синхронізуємо базу даних
+    await sequelize.sync({ force: true });
+    console.log("Базу даних синхронізовано");
+
+    // Створення нового студента
+    const newStudent = {
+      firstName: "Jon",
+      lastName: "Down",
+      email: "down1@gmail.com",
+      birthday: "1998-12-23",
+      isMale: true, // виправлено з isNale
+    };
+
+    const createdStudent = await Student.create(newStudent);
+    console.log(createdStudent.get());
+  } catch (err) {
+    console.error("Помилка:", err);
+  }
+})();
